710e7c152874114752446fa190d42eff4d0d55c2
Tachera Sasi <tacherasasi@gmail.com>
2025-02-14 01:13:03 +0300

    Update home route to display welcome view instead of redirecting to login

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect("/login");
-    // return view('welcome');
+    return view('welcome');
 });
 
 Route::get('/dashboard', [ProfileController::class,'index'])->middleware(['auth', 'verified'])->name('dashboard');
