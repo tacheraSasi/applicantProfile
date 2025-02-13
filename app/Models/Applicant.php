a5f1c3ef7cf7024746aa5a2ccea22ba02e2a3d7b
Tachera Sasi <tacherasasi@gmail.com>
2025-02-13 09:11:47 +0300

    Add fillable attributes and relationships to Applicant model

--- app/Models/Applicant.php
+++ app/Models/Applicant.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -8,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Applicant extends Model
 {
     use HasFactory;
+    
+    protected $fillable = ['full_name', 'email', 'phone'];
+
+    public function education()
+    {
+        return $this->hasMany(Education::class);
+    }
+
+    public function experience()
+    {
+        return $this->hasMany(Experience::class);
+    }
+
+    public function skills()
+    {
+        return $this->hasMany(Skill::class);
+    }
 }
